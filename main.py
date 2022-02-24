892e72d1ebd976b0606ccf8205f727f8ad02f7d8
prygres-technica <prygres.technica@protonmail.com>
2022-02-24 14:59:20 +0100

    add main.py and requirements.txt

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import time
 import json
 import requests
 import threading
+from os import getenv
+from timeloop import Timeloop
+from datetime import timedelta
 from databox import Client
+
+client = Client()														# инициализируем клиент databox с токеном пользователя
+tl = Timeloop()
 
 def encode_output_data(input_json):
 	output_list = []
@@ -11,28 +17,22 @@ def encode_output_data(input_json):
 
 	return output_list
 
+@tl.job(interval=timedelta(seconds=getenv(DATABOX_UPDATE_INTERVAL)))
 def parse_and_push():
-	response = requests.get('https://combot.org/c/-707109232/a/json') 	# получаем данные в от combot
+	response = requests.get(getenv(COMBOT_LINK)) 						# получаем данные в от combot
 	if response.ok == False:											# проверяем получены ли данные, если нет - выдаем ошибку и заканчиваем выполнение функции
 		print(f'Combot data parse error. Status code: {response.status_code}')
 		return
-	else:
-		print(response.status_code)
 
 	json_data = json.loads(response.text) 								# сериализируем данные эти данные
-	json_data.popitem() 												# удаляем послейний элемент (time_stamp) из сериализированых данных
+	json_data.popitem() 												# удаляем послейний элемент, time_stamp из сериализированых данных
 
 	data_to_send = encode_output_data(json_data) 						# "кодируем" данные на отправку
 	pushId = client.insert_all(data_to_send) 							# отправляем данные в databox
-	print (f"Push id: {pushId}\nTime: {time.time()}") 					# выводим в консоль id и время транзакции
+	print (f"Push id: {pushId}\tTime: {time.time()}") 					# выводим в консоль id и время транзакции
 
-#def main():
-#	print(time.ctime())
-#	threading.Timer(10, parse_and_push).start()
-
-client = Client()														# инициализируем клиент databox с токеном пользователя
-parse_and_push()
-#main()
+if __name__ == "__main__":
+	tl.start(block=True)
 
 
 
