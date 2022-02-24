1281c0c6c158efc5a2d91eb18a7bd65f40ba9980
prygres-technica <prygres.technica@protonmail.com>
2022-02-24 16:40:19 +0100

    migrate from timeloop to apscheduler

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import json
 import requests
 import threading
 from os import getenv
-from timeloop import Timeloop
-from datetime import timedelta
 from databox import Client
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-client = Client()														# инициализируем клиент databox с токеном пользователя
-tl = Timeloop()
+client = Client()		# инициализируем клиент databox с токеном пользователя
+sched = BlockingScheduler()
 
 def encode_output_data(input_json):
 	output_list = []
@@ -16,24 +15,19 @@ def encode_output_data(input_json):
 		output_list.append({'key': key, 'value': value})
 	return output_list
 
-@tl.job(interval=timedelta(seconds=int(getenv("DATABOX_UPDATE_INTERVAL"))))
+@sched.scheduled_job('interval', minutes=int(getenv("DATABOX_UPDATE_INTERVAL")))
 def parse_and_push():
-	response = requests.get(getenv("COMBOT_LINK")) 						# получаем данные в от combot
-	if response.ok == False:											# проверяем получены ли данные, если нет - выдаем ошибку и заканчиваем выполнение функции
+	response = requests.get(getenv("COMBOT_LINK"))		# получаем данные в от combot
+	if response.ok == False:		# проверяем получены ли данные, если нет - выдаем ошибку и заканчиваем выполнение функции
 		print(f'Combot data parse error. Status code: {response.status_code}')
 		return
 
-	json_data = json.loads(response.text) 								# сериализируем данные эти данные
-	json_data.popitem() 												# удаляем послейний элемент, time_stamp из сериализированых данных
+	json_data = json.loads(response.text)		# сериализируем данные эти данные
+	json_data.popitem()		# удаляем послейний элемент, time_stamp из сериализированых данных
 
-	data_to_send = encode_output_data(json_data) 						# "кодируем" данные на отправку
-	pushId = client.insert_all(data_to_send) 							# отправляем данные в databox
-	print (f"Push id: {pushId}\tTime: {time.time()}") 					# выводим в консоль id и время транзакции
+	data_to_send = encode_output_data(json_data)		# "кодируем" данные на отправку
+	pushId = client.insert_all(data_to_send)		# отправляем данные в databox
+	print (f"Push id: {pushId}\tTime: {time.time()}")		# выводим в консоль id и время транзакции
 
 if __name__ == "__main__":
-	tl.start(block=True)
-
-
-
-#s = sched.scheduler(time.time, time.sleep)								# инициализируем scheduler
-#s.enter(5000, 1, parse_and_push)										# scheduler будет вызывать parse_and_push каждые 5 мин
+	sched.start()
